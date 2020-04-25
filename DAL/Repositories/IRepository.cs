e1da1ffe11454e67bae313022e852c92d5e78fca
Сергей Бышенко <casion@bigmir.net>
2020-04-25 15:12:43 +0300

    Implementing the generic version in the repository

---
+++ DAL/Repositories/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DAL.Repository.Interfaces
+{
+    public interface IRepository<T>
+    {
+        void Add(T item);
+        void Delete(Guid id);
+        void Edit(T item, Guid id);
+        Task<T> FindModelAsync(Guid id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetAsync(Guid id);
+    }
+}
---
+++ DAL/Repositories/Repository.cs
@@ -0,0 +1,60 @@
+﻿using DAL.Context;
+using DAL.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DAL.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private LibraryDataBaseContext _context;
+
+        public Repository(LibraryDataBaseContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(T item)
+        {
+            _context.Set<T>().Add(item);
+        }
+
+        public void Delete(Guid id)
+        {
+            var model = FindModelAsync(id).Result;
+            if (model == null)
+            {
+                throw new NullReferenceException($"{typeof(T)} not found");
+            }
+            _context.Entry<T>(model).State = EntityState.Deleted;
+            _context.Set<T>().Remove(model);
+        }
+        public void Edit(T item, Guid id)
+        {
+            var model = FindModelAsync(id).Result;
+            if (model == null)
+            {
+                throw new NullReferenceException($"{typeof(T)} not found");
+            }
+            Delete(id);
+            Add(item);
+        }
+        public async Task<T> FindModelAsync(Guid id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+        public async Task<T> GetAsync(Guid id)
+        {
+            return await FindModelAsync(id);
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            var task = Task.Run(() => _context.Set<T>());
+            return await task;
+        }
+
+    }
+}
